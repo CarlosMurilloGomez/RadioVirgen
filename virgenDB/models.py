8c21bdb25aba330a729a49e711668eaaa192b424
ivan152005 <ivangonzalezbartolome@gmail.com>
2025-01-30 10:19:52 +0100

    commit

--- virgenDB/models.py
+++ virgenDB/models.py
@@ -7,16 +7,36 @@ class Autor(models.Model):
     def __str__(self):
         return self.nombre
 
+class Plan(models.Model):
+
+class Podcast(models.Model):
+
+
+class Programa(models.Model):
+    nombre = models.CharField(max_length=100)
+    autores = models.ManyToManyField(Autor, related_name='programas')
+    def __str__(self):
+        return self.nombre
+
+
+
 class Familia(models.Model):
     nombre = models.CharField(max_length=100)
-    miembros = models.ManyToManyField(Usuario, related_name='familias')
     max_miembros = models.IntegerField(default=4)
     def __str__(self):
         return self.nombre
 
 
-class Programa(models.Model):
-    nombre = models.CharField(max_length=100)
-    autor = models.ForeignKey(Autor, on_delete=models.CASCADE, related_name='programas')
-    def __str__(self):
-        return self.nombre
+class Usuario(models.Model):
+    nick = models.CharField(unique=True)
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=40)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=30)
+    fecha_nacimiento = models.DateField(null=True, blank=True)
+    fecha_de_baja = models.DateField(null=True, blank=True)
+    plan = models.ForeignKey(Programa, on_delete=models.CASCADE, related_name="usuarios", null=True, blank=True)
+    familia = models.ForeignKey(Familia, on_delete=models.CASCADE, related_name="usuarios", null=True, blank=True)
+    podcast_pendientes = models.ManyToManyField(Podcast, related_name="usuarios", blank=True)
+    me_gusta_podcast = models.ManyToManyField(Podcast, related_name="usuarios", blank=True)
+    podcast_reproducciones = models.JSONField(default=dict)
