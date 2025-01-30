31f4e21536eb71fed2c6a7442ffb3c2aeac74964
damdepm <damdepm@gmail.com>
2025-01-30 10:03:41 +0100

    clases autor, programa, familia

--- virgenDB/models.py
+++ virgenDB/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    def __str__(self):
+        return self.nombre
+
+class Familia(models.Model):
+    nombre = models.CharField(max_length=100)
+    miembros = models.ManyToManyField(Usuario, related_name='familias')
+    max_miembros = models.IntegerField(default=4)
+    def __str__(self):
+        return self.nombre
+
+
+class Programa(models.Model):
+    nombre = models.CharField(max_length=100)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE, related_name='programas')
+    def __str__(self):
+        return self.nombre
